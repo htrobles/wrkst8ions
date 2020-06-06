a06dd420ea857979fb8cab45aa60d2733a216a4d
Hector Robles <hjt.robles@gmail.com>
2020-06-06 23:46:39 +0800

    Updates

---
+++ precache-manifest.305f466944a23a7fe727d2ec7e9525fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df180af6dff0e102dcfab95d73d0947",
+    "url": "/wrkst8ions/index.html"
+  },
+  {
+    "revision": "98e9e685d86d350eeff7",
+    "url": "/wrkst8ions/static/js/2.2c5521e3.chunk.js"
+  },
+  {
+    "revision": "3bf6c32fa60fa0f7b41615b984399950",
+    "url": "/wrkst8ions/static/js/2.2c5521e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52c6e9948d2490a582ab",
+    "url": "/wrkst8ions/static/js/main.d04c8da1.chunk.js"
+  },
+  {
+    "revision": "fc7ee7f491c1c7741508",
+    "url": "/wrkst8ions/static/js/runtime-main.0215e34a.js"
+  }
+]);
